fb88c2be5c1ef0a65025325c08c4265beed35a44
shakhoond <shayan.akhoond@gmail.com>
2016-06-28 11:38:56 +1200

    Updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var xhr = require('xhr')
 var greeting = require('./views/greeting.hbs')
 
-var endpoint = 'https://api.wheretheiss.at/v1/satellites/255'
+var endpoint = 'https://api.wheretheiss.at/v1/satellites/25544'
 // console.log(endpoint)
 
 xhr.get(endpoint, function (err, data) {
