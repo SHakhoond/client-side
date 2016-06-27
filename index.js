8c7578c26beab426bc421ea1a300066b490b38f3
shakhoond <shayan.akhoond@gmail.com>
2016-06-28 11:31:16 +1200

    Updates

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var xhr = require('xhr')
 var greeting = require('./views/greeting.hbs')
 
-var endpoint = 'https://api.wheretheiss.at/v1/satellites'
+var endpoint = 'https://api.wheretheiss.at/v1/satellites/255'
+// console.log(endpoint)
 
 xhr.get(endpoint, function (err, data) {
   if (err) {
@@ -9,7 +10,7 @@ xhr.get(endpoint, function (err, data) {
   }
 
   // In case you're curious
-  console.log(data.body) // FYI: data.body is a string
+  console.log(JSON.parse(data.body)) // FYI: data.body is a string
 
   // Replace 'Space' below with the response
   var target = document.getElementsByTagName('main')[0]
