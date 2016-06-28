461d54e71c5a5fd55f62124238df7c7dd07fba4a
shakhoond <shayan.akhoond@gmail.com>
2016-06-28 17:15:43 +1200

    Updates

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var xhr = require('xhr')
-var greeting = require('./views/greeting.hbs')
+var example = require('./views/example.hbs')
 
 xhr.get('https://api.wheretheiss.at/v1/satellites/25544', function(err, data) {
   if (err) console.log(err) // do something
 
   data = JSON.parse(data.body)
-  console.log(data.velocity)
-  document.body.innerHTML = greeting({ name: data.velocity });
+  console.log(data)
+  document.body.innerHTML = example({ name: data.velocity });
 })
--- re2.js
+++ re2.js
@@ -2,21 +2,23 @@ var xhr = require('xhr')
 var newPage = require('./views/res2bundle.hbs')
 
 var data = {
-  url: '192.168.1.246:3000/v1/teams',
-  useXDR: true,
-  method: 'GET'
+  url: 'https://api.wheretheiss.at/v1/satellites/25544'
 }
+
+document.getElementById('clicky').addEventListener('click', myFunction)
+
 // console.log(endpoint)
+function myFunction(){
+  xhr(data, function (err, data) {
+    if (err) {
+      console.error(err)
+    }
 
-xhr(data, function (err, data) {
-  if (err) {
-    console.error(err)
-  }
-
-  // In case you're curious
-  console.log(data) // FYI: data.body is a string
-
-  // Replace 'Space' below with the response
-  
-  document.body.innerHTML = newPage({teams: 'data'})
-})
+    // In case you're curious
+    console.log(JSON.parse(data.body)) // FYI: data.body is a string
+    data = JSON.parse(data.body)
+    // Replace 'Space' below with the response
+    
+    document.querySelector("main").innerHTML = newPage(data)
+  })
+}
